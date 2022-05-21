756ad9fc0de7c7c483af12c2a0300c8a62d24a10
KylieNamugga <96869130+KylieNamugga@users.noreply.github.com>
2022-05-21 17:08:39 +0300

    first test on 2*2 failed, second passed

--- test/test.js
+++ test/test.js
@@ -8,4 +8,9 @@ describe('Testing our multiplication',() =>{
         assert.equal(multiply(1, 1), 1);
     });
 
+    it('should multiply 2 and 2 to get 4', () => {
+        assert.equal(multiply(2, 2), 4);
+    });
+
+
 })
